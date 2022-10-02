43bc86b373f661f27617b3da6923ea187f54dee9
steven-yn <steven_yoon1009@naver.com>
2022-10-02 11:39:02 +0900

    socket

--- src/index.ts
+++ src/index.ts
@@ -1,109 +1,33 @@
-import { promises as dns, ADDRCONFIG, V4MAPPED } from "dns";
-import tls from "tls";
-import cheerio from "cheerio";
-import { Socket } from "dgram";
-import { send } from "process";
+import net from "net";
 
-// function whenReceive(socket: tls.TLSSocket): Promise<string> {
-//   return new Promise((resolve) => {
-//     let data = "";
-//     let first = true;
-//     socket.on("data", (chunk: any) => {
-//       if (first) {
-//         // 첫 청크는 무조건 헤더이므로 거름
-//         first = false;
-//         return;
-//       }
-//       // 이후부터 Body 즉 HTML만 받음
-//       data += chunk;
-//     });
-//     socket.once("end", () => {
-//       socket.end();
-//       resolve(data);
-//     });
-//   });
-// }
+const server = net.createServer((socket) => {
+  console.log("connected.");
 
-async function getIP(): Promise<{ host: string; address: string }> {
-  const host = "www.google.com";
-  const { address } = await dns.lookup(host, {
-    family: 4,
-    hints: ADDRCONFIG | V4MAPPED,
-  });
-
-  // console.log(host, address);
-
-  return { host: host, address: address };
-}
-
-function getResponse(socket: tls.TLSSocket): Promise<string> {
-  return new Promise((resolve) => {
-    let data = "";
-    let first = true;
-    socket.on("data", (chunk) => {
-      // console.log(chunk);
-
-      if (first) {
-        // 첫 청크는 무조건 헤더이므로 거름
-        first = false;
-        return;
-      }
-      // 이후부터 Body 즉 HTML만 받음
-      data += chunk;
-    });
-
+  socket.on("data", (data) => {
     console.log(data);
-
-    socket.once("end", () => {
-      console.log("dd");
-      socket.end();
-      resolve(data);
-    });
   });
-}
 
-const ex = async () => {
-  const { host, address } = await getIP();
-  let out = "";
+  socket.on("close", () => {
+    console.log("client disconnted.");
+  });
 
-  const HTTP_CALL = async () => {
-    socket.write(
-      [
-        "GET /search?q=hello&hl=ko HTTP/1.1",
-        `Host: ${host}`,
-        "User-Agent: HereAgent",
-        "\n",
-      ].join("\n")
-    );
+  const resBody = "<html> <head></head> <body>hello world!</body> </html>";
 
-    // console.log(await getResponse(socket), "soc");
-
-    out = await getResponse(socket);
-  };
-
-  const socket = tls.connect(
-    {
-      host: address,
-      port: 443,
-      rejectUnauthorized: false,
-    },
-    HTTP_CALL
+  socket.write(
+    [
+      "HTTP/1.1 200 OK",
+      `Content-Type: text/html;charset=UTF-8
+      Content-Length: ${resBody.length}`,
+      "\n",
+      `${resBody}`,
+    ].join("\n")
   );
-
-  return out;
-};
-
-import express, { Request, Response, NextFunction } from "express";
-
-const app = express();
-
-app.get("/", async (req: Request, res: Response, next: NextFunction) => {
-  const sending = await ex();
-
-  console.log(sending);
-
-  res.send("loading");
-  await res.send(sending);
 });
 
-app.listen("1234");
+server.on("error", (err) => {
+  console.log("err" + err);
+});
+
+server.listen(5000, () => {
+  console.log("linstening on 5000..");
+});
